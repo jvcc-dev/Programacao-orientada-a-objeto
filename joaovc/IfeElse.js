53e7fec01361501c51987b195c517e8232486d51
João Victor Cunha <81644552+jvcc-dev@users.noreply.github.com>
2025-02-17 12:56:52 -0300

    introdução a C e Java
    Aula sobre if e else em C

---
+++ joaovc/IfeElse.js
@@ -0,0 +1,20 @@
+//codigo de if e else
+// Vetor rm uma variavel que pode-se armazenar N valores do mesmo tipo
+
+        var Cont = 0 , Maior,Menor;
+        var Valor = [10];
+       
+           for(Cont = 0; Cont < Valor ; Cont++){
+       
+               var Valor = (Valor + Cont);
+               if(Valor[Cont]>= Valor[Cont+1]){
+                   Valor[Cont] = Maior
+                   Valor[Cont+1] = Menor
+               }else{
+                   Valor[Cont] = Menor
+                   Valor[Cont+1] = Maior
+               }
+       
+           
+           }   
+ 
---
+++ joaovc/newFile.js
@@ -0,0 +1,19 @@
+//codigo de if e else
+// Vetor rm uma variavel que pode-se armazenar N valores do mesmo tipo
+
+var Cont = 0 , Maior,Menor;
+var Valor = [10];
+Maior = Valor[0];
+Menor = Valor[0];
+
+   for(Cont = 0; Cont < Valor[Cont] ; Cont++){
+
+       var Valor = (Valor + Cont);
+       if(Valor[Cont]>= Maior){
+           Valor[Cont] = Maior
+       }else if (Valor[Cont]<= Menor){
+           Valor[Cont] = Menor
+       }
+
+   
+   }   
