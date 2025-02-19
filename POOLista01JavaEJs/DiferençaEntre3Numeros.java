0cd5803e996f0bc187bd7dd7d6b66b01be7af982
João Victor Cunha <81644552+jvcc-dev@users.noreply.github.com>
2025-02-19 12:55:10 -0300

    Primeira lista de questões
    Poo Introdução a Java

---
+++ POOLista01JavaEJs/DiferençaEntre3Numeros.java
@@ -0,0 +1,11 @@
+
+ public interface DiferençaEntre3Numeros {
+    public static void main(String[] args) {
+        int Vetor[3], Maior, Menor, Cont;
+ 
+        for(Cont=0)
+        
+    }
+ 
+ }       
+    
